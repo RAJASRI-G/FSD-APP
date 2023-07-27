263c4d6d037ea246deb7024fc00cb899456fc212
RAJASRI-G <125953572+RAJASRI-G@users.noreply.github.com>
2023-07-27 10:15:44 +0530

    Delete Calendar directory

--- Calendar/conts.js
+++
@@ -1,9 +0,0 @@
-export const DAYS = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
-
-export const MOCKAPPS = [
-  { date: new Date(2023, 6, 3), title: "DB Integration", color: "lightpink" },
-  { date: new Date(2023, 6, 6), title: "Front End Development", color: "hotpink" },
-  { date: new Date(2023, 6, 25), title: "Testing", color: "Midnightblue" },
-  { date: new Date(2023, 6, 15), title: "Implementation of Dockers", color: "purple" },
-  { date: new Date(2023, 7, 3), title: "Setup Service Discovery and Communication", color: "purple" }
-];
